a1d8f8ec3b59be435f37c70d993c144449560d4d
hagyuho <hagyuho90@gmail.com>
2022-07-20 17:30:57 +0900

    javascript web frontend assignment test / like react

---
+++ components/Suggestion.js
@@ -0,0 +1,80 @@
+export default function Suggestion({ $target, initialState, onSelect }) {
+  this.$element = document.createElement("div");
+  this.$element.className = "Suggestion";
+  $target.appendChild(this.$element);
+
+  this.state = {
+    selectedIndex: 0,
+    items: initialState.items,
+  };
+
+  this.setState = (nextState) => {
+    this.state = {
+      ...this.state,
+      ...nextState,
+    };
+    this.render();
+  };
+
+  this.render = () => {
+    const { items = [], selectedIndex } = this.state;
+    if (items.length > 0) {
+      this.$element.style.display = "block";
+      this.$element.innerHTML = `
+              <ul>
+               ${items
+                 .map(
+                   (item, index) => `
+               <li class="${
+                 index === selectedIndex ? "Suggestion__item--selected" : ""
+               }" data-index = "${index}">${item}</li> 
+               `
+                 )
+                 .join("")}
+              </ul>
+              `;
+    } else {
+      this.$element.style.display = "none";
+      this.$element.innerHTML = "";
+    }
+  };
+
+  this.render();
+
+  window.addEventListener("keyup", (e) => {
+    if (this.state.items.length > 0) {
+      const { selectedIndex } = this.state;
+      const lastIndex = this.state.length - 1;
+      const navigationKeys = ["ArrowUp", "ArrowDown"];
+      let nextIndex = selectedIndex;
+
+      if (navigationKeys.includes(e.key)) {
+        console.log(e.key);
+        if (e.key === "ArrowUp") {
+          nextIndex = selectedIndex === 0 ? lastIndex : nextIndex - 1;
+        } else if (e.key === "ArrowDown") {
+          nextIndex = selectedIndex === lastIndex ? 0 : nextIndex + 1;
+        }
+        this.setState({
+          ...this.state,
+          selectedIndex: nextIndex,
+        });
+      } else if (e.key === "Enter") {
+        console.log("enter");
+        onSelect(this.state.items[this.state.selectedIndex]);
+      }
+    }
+  });
+
+  this.$element.addEventListener("click", (e) => {
+    const $li = e.target.closest("li");
+    if ($li) {
+      const { index } = $li.dataset;
+      try {
+        onSelect(this.state.items[parseInt(index)]);
+      } catch (e) {
+        alert("선택불가!!!");
+      }
+    }
+  });
+}
